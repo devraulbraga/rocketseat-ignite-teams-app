7ababb03971f77da1ee237a97ac0a070d2f66e8c
Raul Braga <raulbraga@hotmail.com>
2024-07-24 16:46:31 -0300

    Alteração do tema do status bar

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ export default function App() {
   });
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar style="light"/>
+      <StatusBar style="dark"/>
       {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
